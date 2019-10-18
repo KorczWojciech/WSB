7de0b254326661694723414dea674f99a5c4cb7b
KorczWojciech <wojciech.korcz99@gmail.com>
2019-10-18 19:21:56 +0200

    testowe dodanie z domu

---
+++ Podstawy_programowania/dom1/dom1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projekt testowy");
+            Console.WriteLine("czy to na pewno działa?");
+
+            Console.ReadKey();
+        }
+    }
+}
